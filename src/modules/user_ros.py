20acbd2f07272ce676c6127f77c5ddc92b76c65f
Doncey Albin <donceyalbin@gmail.com>
2023-10-05 13:12:54 -0600

    Adapted for regular lio-sam

--- src/modules/user_ros.py
+++ src/modules/user_ros.py
@@ -135,7 +135,7 @@ class User():
 
         pointcloud2_sub = message_filters.Subscriber('/ouster/points', PointCloud2)
         #posestamped_sub = message_filters.Subscriber('/sc_lio_sam/pose_estimate', PoseStamped)
-        odom_sub = message_filters.Subscriber('/sc_lio_sam/mapping/odometry', Odometry)
+        odom_sub = message_filters.Subscriber('/lio_sam/mapping/odometry', Odometry)
 
         #ts = message_filters.ApproximateTimeSynchronizer([pointcloud2_sub, posestamped_sub], queue_size=10, slop=0.01)
         ts = message_filters.ApproximateTimeSynchronizer([pointcloud2_sub, odom_sub], queue_size=10, slop=0.01)
